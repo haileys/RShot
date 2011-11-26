1570d88a51122d61eaea9ac488e8ebd4e5b2e6f2
charliesome <charlie@charliesomerville.com>
2011-11-26 21:58:17 +1100

    spaces and tabs lol

--- ext/rshot/ruby.m
+++ ext/rshot/ruby.m
@@ -16,36 +16,36 @@ static AppDelegate* app_delegate;
 
 void Init_rshot()
 {
-	pool = [[NSAutoreleasePool alloc] init];
-	[NSApplication sharedApplication];
+    pool = [[NSAutoreleasePool alloc] init];
+    [NSApplication sharedApplication];
     app_delegate = [[AppDelegate alloc] init];
     [NSApp setDelegate: app_delegate];
-	
-	rb_cRShot = rb_define_class("RShot", rb_cObject);
-	rb_define_alloc_func(rb_cRShot, RShot_alloc);
-	rb_define_method(rb_cRShot, "render_url", RShot_render_url, 2);
+
+    rb_cRShot = rb_define_class("RShot", rb_cObject);
+    rb_define_alloc_func(rb_cRShot, RShot_alloc);
+    rb_define_method(rb_cRShot, "render_url", RShot_render_url, 2);
 }
 
 static void RShot_free(void* rshot)
 {
-	[(RShot*)rshot release];
-	[pool drain];
+    [(RShot*)rshot release];
+    [pool drain];
 }
 
 static VALUE RShot_alloc(VALUE class)
 {
-	RShot* rshot = [[RShot alloc] init];
-	return Data_Wrap_Struct(class, 0, RShot_free, rshot);
+    RShot* rshot = [[RShot alloc] init];
+    return Data_Wrap_Struct(class, 0, RShot_free, rshot);
 }
 
 static VALUE RShot_render_url(VALUE self, VALUE url, VALUE png)
 {
-	RShot* rshot;
-	NSString* nsurl = [NSString stringWithUTF8String: rb_string_value_cstr(&url)];
-	NSString* nspng = [NSString stringWithUTF8String: rb_string_value_cstr(&png)];
-	Data_Get_Struct(self, RShot, rshot);
-	[rshot renderUrl: nsurl toFile: nspng];
-	if(!setjmp(RShot_finishedEnv)) {
+    RShot* rshot;
+    NSString* nsurl = [NSString stringWithUTF8String: rb_string_value_cstr(&url)];
+    NSString* nspng = [NSString stringWithUTF8String: rb_string_value_cstr(&png)];
+    Data_Get_Struct(self, RShot, rshot);
+    [rshot renderUrl: nsurl toFile: nspng];
+    if(!setjmp(RShot_finishedEnv)) {
         [NSApp run];
     }
     return Qtrue;
